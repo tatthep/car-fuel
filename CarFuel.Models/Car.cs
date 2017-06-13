874e358431263155f5057f5ec66d0db65ab61c2a
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2017-06-13 15:56:48 +0700

    Init

---
+++ CarFuel.Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuel.Models {
+  public class Car {
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+    public string PlateNo { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int EngineSizeCC { get; set; }
+
+    public DateTime DateAdded { get; set; }
+    public string OwnerId { get; set; }
+
+  }
+}
---
+++ CarFuel.Services.Facts/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuel.Services.Facts {
+  public class Class1 {
+  }
+}
