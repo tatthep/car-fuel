280fd88d9038fb2ad2436a47c57f79e5e6f8c0c2
panphia <pandy.9@hotmail.com>
2017-06-14 14:52:58 +0700

    Update Models (Car and FillUp)

--- CarFuel.Models/Car.cs
+++ CarFuel.Models/Car.cs
@@ -6,38 +6,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarFuel.Models {
-  public class Car {
+namespace CarFuel.Models
+{
+    public class Car
+    {
 
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public Guid Id { get; set; }
-    
-    [StringLength(20)]
-    public string Name { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
 
-    [Display(Name="Plate No.")]
-    [StringLength(20)]
-    public string PlateNo { get; set; }
+        [StringLength(20)]
+        public string Name { get; set; }
 
-    [Required]
-    [StringLength(30)]
-    public string Make { get; set; }
+        [Display(Name = "Plate No.")]
+        [StringLength(20)]
+        public string PlateNo { get; set; }
 
-    [Required]
-    [StringLength(30)]
-    public string Model { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Make { get; set; }
 
-    [Range(0, 9999)]
-    public int Year { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Model { get; set; }
 
-    [Display(Name="Engine Size (cc.)")]
-    [Range(0, 9999)]
-    public int? EngineSizeCC { get; set; }
+        [Range(0, 9999)]
+        public int Year { get; set; }
 
-    public DateTime DateAdded { get; set; }
+        [Display(Name = "Engine Size (cc.)")]
+        [Range(0, 9999)]
+        public int? EngineSizeCC { get; set; }
 
-    [StringLength(36)]
-    public string OwnerId { get; set; }
+        public DateTime DateAdded { get; set; }
 
-  }
+        [StringLength(36)]
+        public string OwnerId { get; set; }
+
+        public virtual ICollection<FillUp> FillUps { get; set; }
+
+        public Car()
+        {
+            FillUps = new HashSet<FillUp>();
+        }
+
+    }
 }
---
+++ CarFuel.Models/Fillup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuel.Models
+{
+    public class FillUp
+    {
+        public int Id { get; set; }
+        public int Odometer { get; set; }
+        public double Liters { get; set; }
+        public bool IsFull { get; set; }
+
+        public double GetKML {
+            get {
+                return 0;
+            }
+        }
+
+        public virtual Car car { get; set; }
+    }
+}
