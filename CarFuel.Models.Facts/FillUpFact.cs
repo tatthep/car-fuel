fbd27bb2a621eecef84bb8f593ab732fd26a1315
tatthep <vk1.tatthep@gmail.com>
2017-06-14 16:04:06 +0700

    Enable Calculate KilometersPerLiter

---
+++ CarFuel.Models.Facts/FillUpFact.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CarFuel.Models;
+
+namespace CarFuel.Models.Facts {
+    public class FillUpFact {
+
+        [Fact]
+        public void KML_ReturnsNull() {
+            //arrrang
+            FillUp f1 = new FillUp();
+            f1.Odometer = 1000;
+            //act
+            double? result = f1.KilometersPerLiter;
+            //assert
+            Assert.Null(result);
+
+        }
+
+        [Fact]
+        public void SecondFillUp_Return10() {
+            //arrrang
+            FillUp f1 = new FillUp();
+            f1.Odometer = 1000;
+            f1.Liters = 60;
+            FillUp f2 = new FillUp();
+            f2.Odometer = 1500;
+            f2.Liters = 50;
+            f1.NexFillUp = f2;
+
+            //act
+            double? result = f1.KilometersPerLiter;
+
+            //assert
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void ThirdFillUp_Return12() {
+            //arrrang
+            FillUp f1 = new FillUp();
+            f1.Odometer = 1000;
+            f1.Liters = 60;
+
+            FillUp f2 = new FillUp();
+            f2.Odometer = 1500;
+            f2.Liters = 50;
+
+            f1.NexFillUp = f2;
+
+            FillUp f3 = new FillUp();
+            f3.Odometer = 2100;
+            f3.Liters = 50;
+
+            f2.NexFillUp = f3;
+
+            //act
+            double? result = f2.KilometersPerLiter;
+
+            //assert
+            Assert.Equal(12, result);
+        }
+    }
+}
--- CarFuel.Models/Fillup.cs
+++ CarFuel.Models/Fillup.cs
@@ -4,22 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarFuel.Models
-{
-    public class FillUp
-    {
+namespace CarFuel.Models {
+    public class FillUp {
         public int Id { get; set; }
         public int Odometer { get; set; }
         public double Liters { get; set; }
         public bool IsFull { get; set; }
 
-        public double? KilometersPerLiter
-        {
+        public double? KilometersPerLiter {
             get {
-                return 0;
+                if (NexFillUp == null) {
+                    return null;
+                }
+                else {
+                    return (this.NexFillUp.Odometer - this.Odometer) / NexFillUp.Liters;
+                }
+
             }
         }
 
         public virtual Car Car { get; set; }
+
+        public FillUp NexFillUp { get; set; }
     }
 }
